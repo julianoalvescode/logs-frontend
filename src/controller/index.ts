7966730ea4bdafa6b970f6cef3bd1c458112c4ca
julianoalvescode <contato.julianoalvess@gmail.com>
2023-03-03 14:33:21 -0300

    feat: 🎸 add feature save in localstorage and download

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -12,11 +12,7 @@ class LogsFast implements I.LogsFast {
   public log(params: I.Log): void {
     const isSave = params?.save?.isActivated;
 
-    if (isSave)
-      this.saveLog(
-        `${this.generateLog(params?.type)} \n params?.message`,
-        params?.save?.format || "json"
-      );
+    if (isSave) this.saveLogLocalStorage(params);
 
     console.log(
       "/-------------------------/",
@@ -29,11 +25,58 @@ class LogsFast implements I.LogsFast {
     );
   }
 
-  private saveLog(log: string, format: "text" | "json"): void {
-    try {
-      if (!log) throw new Error("Log is empty");
+  // private saveLog(log: string, format: "text" | "json"): void {
+  //   try {
+  //     if (!log) throw new Error("Log is empty");
+  //     if (format === "text") {
+  //       const blob = new Blob([log], { type: "text/plain" });
+  //       const url = URL.createObjectURL(blob);
+
+  //       const link = document.createElement("a");
+  //       link.setAttribute("href", url);
+  //       link.setAttribute("download", "log.txt");
+  //       link.click();
+  //     } else if (format === "json") {
+  //       const blob = new Blob([JSON.stringify(log)], { type: "text/plain" });
+  //       const url = URL.createObjectURL(blob);
+
+  //       const link = document.createElement("a");
+  //       link.setAttribute("href", url);
+  //       link.setAttribute("download", "log.json");
+  //       link.click();
+  //     }
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // }
+
+  // Save log in local storage
+
+  public saveLogLocalStorage(params: I.Log): void {
+    const isSave = params?.save?.isActivated;
+
+    if (isSave) {
+      const log = `${this.generateLog(params?.type)}: ${params?.message}`;
+
+      const logs = localStorage.getItem("logs");
+      if (logs) {
+        const logsArray = JSON.parse(logs);
+        logsArray.push(log);
+        localStorage.setItem("logs", JSON.stringify(logsArray));
+      } else {
+        localStorage.setItem("logs", JSON.stringify([log]));
+      }
+    }
+  }
+
+  // Get logs from local storage and save in file (text or json)
+  public getLogLocalStorage(format: "text" | "json"): void {
+    const logs = localStorage.getItem("logs");
+    if (logs) {
+      const logsArray = JSON.parse(logs);
+
       if (format === "text") {
-        const blob = new Blob([log], { type: "text/plain" });
+        const blob = new Blob(logsArray, { type: "text/plain" });
         const url = URL.createObjectURL(blob);
 
         const link = document.createElement("a");
@@ -41,7 +84,9 @@ class LogsFast implements I.LogsFast {
         link.setAttribute("download", "log.txt");
         link.click();
       } else if (format === "json") {
-        const blob = new Blob([JSON.stringify(log)], { type: "text/plain" });
+        const blob = new Blob([JSON.stringify(logsArray)], {
+          type: "text/plain",
+        });
         const url = URL.createObjectURL(blob);
 
         const link = document.createElement("a");
@@ -49,8 +94,6 @@ class LogsFast implements I.LogsFast {
         link.setAttribute("download", "log.json");
         link.click();
       }
-    } catch (e) {
-      console.log(e);
     }
   }
 }
